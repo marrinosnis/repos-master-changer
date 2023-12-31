d79f6cb0f0bb852b32d52a5f1ba05e9564f8e82b
marinos <mr.nisiotis@gmail.com>
2023-12-31 16:19:39 +0200

    complete the customCommand option. Works for 'touch' & 'mdkir' commands

--- gitReposEditor.sh
+++ gitReposEditor.sh
@@ -15,7 +15,7 @@ mapChoices=(
     "commit::commitCode"
     "push::pushCode"
     "specific::changeSpecificLine"
-    "customCommand::"
+    "customCommand::customFunc"
 )
 
 # directories=$(find "$rootFolder" -maxdepth 1 -type d \( -name 'ctp-*' -o -name 'ctp.*' \) | wc -l)  # this command prints only the number of the folders, not the names, with the patter 'ctp-' or 'ctp.'
@@ -41,6 +41,7 @@ performAction() {
     shift
     shift
 
+    echo "The argsArray is ${argsArray[@]}"
 
     local customInputPaths="${@:-$ctpDirectories}"
     paths=($customInputPaths)       #make it array, so I can access each element individual
@@ -50,7 +51,7 @@ performAction() {
 }
 
 statusCode(){
-    local pathToLocalRepo=$1
+    local pathToFolder=$1
     if [ "$printOnce" = false ]; then
         echo    "--------------------------------------------------------|"
         echo    "Checking if there are any changes in the repo/s         |"
@@ -58,46 +59,46 @@ statusCode(){
         printOnce=true
     fi
 
-    defaultStatusWithoutChanges=$(git -C "$pathToLocalRepo" status -sb | head -n 1)
-    currentRepoStatus=$(git -C "${pathToLocalRepo}" status --branch --porcelain)
-    currentBranch=$(git -C "$pathToLocalRepo" rev-parse --abbrev-ref HEAD)
+    defaultStatusWithoutChanges=$(git -C "$pathToFolder" status -sb | head -n 1)
+    currentRepoStatus=$(git -C "${pathToFolder}" status --branch --porcelain)
+    currentBranch=$(git -C "$pathToFolder" rev-parse --abbrev-ref HEAD)
 
     if [ "$defaultStatusWithoutChanges" == "$currentRepoStatus" ]; then
         echo -e "The folder in the path:${NO_CHANGES_COLOR}"$path"${END_COLOR} has no changes in the current branch: ${BRANCH_COLOR}"$currentBranch"${END_COLOR}"
     else
         echo -e "\nThe changes in the path: ${CHANGES_COLOR}"$path"${END_COLOR} in the branch ${BRANCH_COLOR}"$currentBranch"${END_COLOR} are\n"
-        git -C "$pathToLocalRepo" status
+        git -C "$pathToFolder" status
     echo "------------------------------------------------------------------------------------------------------------------------"
     echo "------------------------------------------------------------------------------------------------------------------------"
     fi
 }
 
 commitCode(){
-    local pathToLocalRepo=$1
+    local pathToFolder=$1
     shift
 
     local messageArgument=("$@")
     gitCommitMessage="${messageArgument}"
-    git -C "$pathToLocalRepo" commit -S -m "$gitCommitMessage"
+    git -C "$pathToFolder" commit -S -m "$gitCommitMessage"
 }
 
 pushCode(){
-    local pathToLocalRepo=$1
-    git -C "$pathToLocalRepo" push 
+    local pathToFolder=$1
+    git -C "$pathToFolder" push 
 }
 
 changeYAMLRunningMode(){ #specific block of code I mean
-    local pathToLocalRepo=$1
+    local pathToFolder=$1
 
             #          the $ ensures it is at end of the line.
-    sed -i '' -e "/^on:$/s/$/\n  push:/" ${pathToLocalRepo}/.github/workflows/delete-specific-packages.yml   # adds the 'push' under the 'on:'. So with this command we change the status to 'on: push', and can run in every git push in the repo
+    sed -i '' -e "/^on:$/s/$/\n  push:/" ${pathToFolder}/.github/workflows/delete-specific-packages.yml   # adds the 'push' under the 'on:'. So with this command we change the status to 'on: push', and can run in every git push in the repo
             #  the ^ ensures it is in the beginning of the line
 
-    sed -i '' -e "/workflow_dispatch/,/default/s/^/#/" ${pathToLocalRepo}/.github/workflows/delete-specific-packages.yml   # from the 'workflow_dispatch' -until(,)- 'default' word in the .yml -add(s)- in the -beginning(^)- the character #
+    sed -i '' -e "/workflow_dispatch/,/default/s/^/#/" ${pathToFolder}/.github/workflows/delete-specific-packages.yml   # from the 'workflow_dispatch' -until(,)- 'default' word in the .yml -add(s)- in the -beginning(^)- the character #
 }
 
 changeSpecificLine(){
-    local pathToLocalRepo=$1 # I can take only the first one path, because from the loop I will retrieve each time, only 1 path. So I'm ok! 
+    local pathToFolder=$1 # I can take only the first one path, because from the loop I will retrieve each time, only 1 path. So I'm ok! 
     shift
 
     local arguments=("$@")
@@ -105,22 +106,24 @@ changeSpecificLine(){
     oldText="${arguments[1]}"
     newText="${arguments[2]}"
 
-    sed -i '' -e "${lineToChange}s/${oldText}/${newText}/" ${pathToLocalRepo}/.github/workflows/delete-specific-packages.yml  # with this line, I change specific oldText with specific newText
+    sed -i '' -e "${lineToChange}s/${oldText}/${newText}/" ${pathToFolder}/.github/workflows/delete-specific-packages.yml  # with this line, I change specific oldText with specific newText
     # if I want to change the whole line with new line, I have to put the first word of that specific line, in order to keep the indentation, and the newText. The command will be:
     # sed -i '' -e "${lineToChange}s/${oldText}.*/${newText}/" ${directories[17]}/.github/workflows/delete-specific-packages.yml
     # where the ${oldText} should be THE FIRST word of the ${lineToChange}, otherwise it change from the inputed word, until the end of the specific line.
 }
 
 customFunc(){  # change the name of the function. Is not very representative
-    local paths=("$@")
+    local pathToFolder=$1
+    shift
 
-    if [[ "${paths[@]}" -eq 0 ]]; then  
-        echo -e "${WARNING_COLOR}Empty paths variable given. Please try again${END_COLOR}"
-    else
-        for path in "${paths[@]}"; do
-            echo "the path is: $path"
-        done
-    fi
+    local arguments=("$@")
+    command="${arguments[0]}"
+
+    for arg in "${arguments[@]:1}"; do
+        "$command" "$pathToFolder/$arg"
+    done
+
+     
 }
 
 setSpecificPaths(){
@@ -148,7 +151,7 @@ while [[ $# -gt 0 ]]; do
             ;;
 
         --commit)  # should add to take the commit message, and do that for all, or, for the specified projects
-            read -p "Enter the commit message:" commitMessage
+            read -p "Enter the commit message: " commitMessage
             setSpecificPaths
             performAction "commit" "$commitMessage" "${specifiedPaths[@]}" # it gives the commitMessage as the first parameter to performAction. If the paths, are not set, it will apply to all the matches of the pattern
             ;;
@@ -171,8 +174,12 @@ while [[ $# -gt 0 ]]; do
             ;;
 
         --customCommand)  # change the name of the parameter. Is not very representative
+            read -p "Enter the custom command that you want to apply: " inputCmd
+            result=$(echo "$inputCmd" | tr ' ' ',')
+            echo "$result"
             setSpecificPaths
-            performAction "" "${specifiedPaths[@]}"
+
+            performAction "customCommand" "$result" "${specifiedPaths[@]}"
             ;;
         
         *)
