36cff19aca749d15fc26e002ac4cefad45108161
marinos <mr.nisiotis@gmail.com>
2024-02-22 22:28:29 +0200

    update test.sh file

--- tests/test.sh
+++ tests/test.sh
@@ -66,6 +66,9 @@ function testCustomGitStatusFunction(){
     responseTracked="$($script)"
     responseTrackedWithoutColors=$(echo "$responseTracked" | sed 's/\x1B\[[0-9;]*[JKmsu]//g')
     assertEquals "${responseTrackedWithoutColors}" "${mapExpectedMessages[3]}"
+
+    git restore --staged temp.txt
+    rm temp.txt
 }
 
 shift $#
