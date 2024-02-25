3c88156580ec398d71ac8534f8417a39be9dd4ae
marinos <mr.nisiotis@gmail.com>
2024-02-25 22:04:01 +0200

    simplify if statement and explain the stash action in the test case

--- tests/testCustomGitStatusFunction.sh
+++ tests/testCustomGitStatusFunction.sh
@@ -3,15 +3,13 @@ source ./tests/messages.sh
 function oneTimeSetUp(){
     script="./gitReposEditor.sh"
 
-    statusResponse=$(git status)
-
-    if [[ "${statusResponse}" == *"nothing to commit, working tree clean"* ]]; then
-        continue
-    else
-        git stash save -m "perform a temp save"
-    fi
-}
-
+    statusResponse=$(git status)                                                       # Perfrom a 'git status' in order to check if there are any changes
+                                                                                       # if NOT, there is no need for 'git stash' to save the possible changes. 
+    if [[ ! "${statusResponse}" == *"nothing to commit, working tree clean"* ]]; then  # But if there are changes,a temp stash is essential to save the current
+        git stash save -m "perform a temp save"                                        # changes, in order to run the tests, with the expected messages in the assertions. 
+    fi                                                                                 # The current scenario, is supposed to run on CI/CD where no changes would be, as 
+}                                                                                      # everything will be commited. Locally, instead, there might be differences, so in order to run
+                                                                                       # the test locally successful, a stash must be done.
 function testCustomGitStatusFunction(){
     touch temp.txt
     
@@ -29,11 +27,9 @@ function testCustomGitStatusFunction(){
 }
 
 function oneTimeTearDown() {
-    
-    if [[ "${statusResponse}" == *"nothing to commit, working tree clean"* ]]; then
-        continue
-    else
-        git stash pop
+                                                                                       # If there are no changes as in the beggining of the test, then it's crucial to ensure that no stashes
+    if [[ ! "${statusResponse}" == *"nothing to commit, working tree clean"* ]]; then  # are applied: a) if a stash already exists, it should not be applied, preserving the current state
+        git stash pop                                                                  # as it was in the latest commit, b) if there are no stashes, pop action would result in failure
     fi
 }
 
